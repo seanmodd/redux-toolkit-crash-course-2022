030d28c11c87b47f49efc1bed47c2a4aa4d9878b
Sean <seansmodd@gmail.com>
2022-03-22 14:11:39 -0700

    first

--- .eslintrc.js
+++ .eslintrc.js
@@ -80,6 +80,11 @@
 module.exports = {
   extends: ['wesbos'],
   plugins: ['react', 'prettier'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
